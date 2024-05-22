a4206b9cff5b5d556cf1947c364eb7086a6ee287
Shivamt7 <141439600+Shivamt7@users.noreply.github.com>
2024-05-22 18:07:36 +0000

    conversion of datatypes

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+//let score = 33
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+
+// let isLoggedIn = undefined
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+// 1 => true
+// 0 => false
+// "" => false
+// "shivam" => true
+
+
+let someNumber = 7
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
