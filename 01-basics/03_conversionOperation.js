18f2d773fd28236fcaa186c16f35687667e4d84e
Shivamt7 <141439600+Shivamt7@users.noreply.github.com>
2024-05-25 05:18:46 +0000

    conversion operation

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -29,8 +29,56 @@
 // "shivam" => true
 
 
-let someNumber = 7
+// let someNumber = 7
 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+
+// *********************** Operations **********************
+
+let value = 7
+let negValue = -value
+// console.log(negValue)
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+
+// let str1 = "hello"
+// let str2 = "shivam"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4) * 5 % 3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+
+// let num1 , num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+
+
+let x = 7
+const y = ++x
+console.log(x);
