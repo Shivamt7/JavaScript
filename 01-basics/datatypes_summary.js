b727938f70828b6c19189bdadfb5ee6d026eee18
Shivamt7 <141439600+Shivamt7@users.noreply.github.com>
2024-05-27 11:55:40 +0000

    stack and heap

--- 01-basics/datatypes_summary.js
+++ 01-basics/datatypes_summary.js
@@ -10,33 +10,67 @@
 // 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt.
 
 
-const score = 100
-const scoreValue = 100.3 // number
+// const score = 100
+// const scoreValue = 100.3        // number
 
-const isLoggedIn = false // boolean
-const outsideTemp = null // Object
-let userEmail; // undefined
+// const isLoggedIn = false       // boolean
+// const outsideTemp = null       // Object
+// let userEmail;                 // undefined
 
-const id = Symbol('123') // symbol
-const anotherId = Symbol('123') // symbol
-console.log(anotherId === id);
+// const id = Symbol('123')       // symbol
+// const anotherId = Symbol('123') // symbol
+// console.log(anotherId === id);
 
-const bigNumber = 654131841631681486n // bigint
+// const bigNumber = 654131841631681486n // bigint
 
 // Reference (Non Primitve)
-
 // Array, Objects, Funtions
 
 
-const heros = ["shaktiman", "naagraj", "doga"] // object
-let myObj = {
-    name: "shivam", // object
-    age: 25
+// const heros = ["shaktiman", "naagraj", "doga"] // object
+// let myObj = {
+//     name: "shivam",              // object
+//     age: 25
+// }
+
+
+// const myFunstion = function () {
+//     console.log("hello world");  // function object
+// }
+
+// console.log(typeof myFunstion)
+
+
+// **************************************************************
+
+// Stack (Primitve) , Heap (Non-Primitive)
+
+
+// STACK Example:
+
+let myYoutubename = "SinuRocks"
+
+let anotherName = myYoutubename
+anotherName = "Iamtibba7"
+
+console.log(myYoutubename)
+console.log(anotherName)
+
+
+
+// HEAP Example:
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
 
+let userTwo = userOne
 
-const myFunstion = function () {
-    console.log("hello world"); // function object
-}
+console.log(userTwo)
 
-console.log(typeof myFunstion)
+userOne.email = "shivam@google.com"
+userTwo.email = "satyam@google.com"
+
+console.log(userOne)
+console.log(userTwo)
